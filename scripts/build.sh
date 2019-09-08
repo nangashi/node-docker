4c3d82d9e829418f7f07248e6921e5e6e10a3631
nan <tagjmp@gmail.com>
2019-09-08 23:54:58 +0900

    ansibleを追加

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 docker-compose -f docker-compose.yml -f docker-compose.build.yml build
+mkdir -p ansible/roles/app/docker/files/images/
+for image in hoge_web hoge_web-api hoge_redis; do
+    docker save -o ansible/roles/app/docker/files/images/${image}.tar ${image}
+done

1a7d53ea96e702485bacd464b85cb6ef53056b32
nan <tagjmp@gmail.com>
2019-09-09 01:09:45 +0900

    途中

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+APP_ROLE_DIR=ansible/roles/app/docker
+
+# build and save images
 docker-compose -f docker-compose.yml -f docker-compose.build.yml build
-mkdir -p ansible/roles/app/docker/files/images/
+mkdir -p ${APP_ROLE_DIR}/files/images/
 for image in hoge_web hoge_web-api hoge_redis; do
-    docker save -o ansible/roles/app/docker/files/images/${image}.tar ${image}
+    docker save -o ${APP_ROLE_DIR}/files/images/${image}.tar ${image}
 done
+
+# copy docker-compose.yml
+cp docker-compose.yml ${APP_ROLE_DIR}/templates/

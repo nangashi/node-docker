712a17bbb6f05f2e05c4c64fab32558965ff3321
nan <tagjmp@gmail.com>
2019-09-14 22:49:39 +0900

    途中

--- ansible/install.sh
+++ ansible/install.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+export ANSIBLE_CONFIG=$(pwd)
 ansible-playbook -i inventories/staging.yml playbooks/all.yml --extra-vars="@inventories/private.yml" -v
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-APP_ROLE_DIR=ansible/roles/app/docker
+SYSTEM_NAME=portal
+ANSIBLE_APP_DOCKER_ROLE_DIR=ansible/roles/app/docker
+TAG=$(date +%Y%m%d)
 
 # build and save images
 docker-compose -f docker-compose.yml -f docker-compose.build.yml build
-mkdir -p ${APP_ROLE_DIR}/files/images/
-for image in hoge_web hoge_web-api hoge_redis; do
-    docker save -o ${APP_ROLE_DIR}/files/images/${image}.tar ${image}
+mkdir -p ${ANSIBLE_APP_DOCKER_ROLE_DIR}/files/images/
+for image in $(docker images | awk '{ print $1 }' | grep -P "^${SYSTEM_NAME}_"); do
+  echo docker tag ${image} ${image}:${TAG}
+  docker tag ${image} ${image}:${TAG}
+  docker save -o ${ANSIBLE_APP_DOCKER_ROLE_DIR}/files/images/${image}.tar ${image}:${TAG}
 done
+date +%Y%m%d > ${ANSIBLE_APP_DOCKER_ROLE_DIR}/files/images/tag
 
 # copy docker-compose.yml
-cp docker-compose.yml ${APP_ROLE_DIR}/templates/
+cp docker-compose.yml ${ANSIBLE_APP_DOCKER_ROLE_DIR}/templates/
